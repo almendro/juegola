b156cacfcbc8bd89dcfe3ffa9480cd7a8599a278
Ernesto Bazzano <bazza@riseup.net>
2012-08-18 02:50:55 -0300

    version funcional de sinapsis.js

--- juegos/parche.js
+++ juegos/parche.js
@@ -21,4 +21,43 @@ function include (filename) {
         loadfile (filename, "js")
 }
 
-trace = alert;
+// trace
+var w = window.wrappedJSObject || window;
+trace = w.alert;
+
+
+// chr y asc http://jalaj.net/2007/03/08/asc-and-chr-in-javascript/
+
+function asc(String) {
+        return String.charCodeAt(0);
+}
+
+function chr(AsciiNum) {
+        return String.fromCharCode(AsciiNum)
+}
+
+
+_root = $("body");
+_level0 = $("body");
+
+
+
+function keyListener(e){
+if(!e){
+        //for IE
+        e = window.event;
+}
+
+if(e.keyCode==37 && paddleLeft > 0){
+//keyCode 37 is left arrow
+paddleLeft -= 4;
+paddle.style.left = paddleLeft + 'px';
+}
+if(e.keyCode==39 && paddleLeft < 436){
+//keyCode 39 is right arrow
+paddleLeft += 4;
+paddle.style.left = paddleLeft + 'px';
+}
+// FYI - keyCode 38 is up arrow,
+// keyCode 40 is down arrow
+}
