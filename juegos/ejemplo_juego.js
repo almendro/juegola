8c26c379cb557f4e8809c1924b645a63199a35e5
almendro <martin@ensamble.com.ar>
2012-08-18 02:20:37 -0300

    estilos para fichas, pruebas con figuras geométricas usando un sólo PNG gigante. Las figuras dispuestas en una grilla de 256x256. Hay un interval calculando las proporciones al cambio de escala.

--- juegos/ejemplo_juego.js
+++ juegos/ejemplo_juego.js
@@ -1,15 +1,24 @@
-trace = function(e){
-	alert(e);
-}
-
-
 var $fichas;
+var render_fichas;
 
 fn_inicio = function (){
-
-	trace ("empezamos");
-
-	$fichas = jQuery('.ficha')
-
+	
+	/* ajustamos el desplazamiento de la imagen segun la numero de ficha, y mantenemos la proporción cuadrada de las fichas */
+	
+	$fichas = jQuery('.ficha');
+	
+	render_fichas = setInterval(function(){
+		$fichas.each(function(e){
+			jQuery(this).css({
+				height: jQuery(this).css('width')
+			});
+			jQuery('.figura img',this).css({
+				top: -e * (jQuery('.figura img',this).height()/11)+'px',
+				left: -e * (jQuery('.figura img',this).width()/11)+'px'
+			});
+		});
+	},200);
+	
 	return false;
+	
 }
